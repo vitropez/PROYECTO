51e3a3f980142bd32c1c45c9d97d8943df9691fc
vitropez <64587299+vitropez@users.noreply.github.com>
2020-11-19 09:23:28 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,7 @@
+const foldersController = require('./folders-controller');
+const UsersController = require('./users-controller');
+
+module.exports = {
+  foldersController,
+  UsersController,
+};
