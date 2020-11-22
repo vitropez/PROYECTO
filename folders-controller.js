106f765d6cad39933c879e728cfc7abdcaa8923f
vitropez <64587299+vitropez@users.noreply.github.com>
2020-11-22 22:13:34 +0100

    Add files via upload

--- folders-controller.js
+++ folders-controller.js
@@ -17,16 +17,16 @@ async function createFolder(req, res) {
     res.status(404);
     return res.send({ error: 'Usuario no encontrado' });
     }
- 
+   
+    const ruta = path.join(__dirname,`./carpetas_usuarios/${nombre}`);
+     fs.mkdir(ruta);
   
-  const insertQuery = 'INSERT INTO carpetas (nombre, id_usuarios,id_carpetas) VALUES (?, ?,?)';
-  const [result]=await database.pool.query(insertQuery,[nombre, userId,carpetasId]);
+  const insertQuery = 'INSERT INTO carpetas (nombre, id_usuarios,id_carpetas,ruta) VALUES (?, ?,?,?)';
+  const [result]=await database.pool.query(insertQuery,[nombre, userId,carpetasId,ruta]);
 
  
  
-    
-    const ruta = path.join(__dirname,`./carpetas_usuarios/${nombre}`);
-     fs.mkdir(ruta);
+   
     
     
  
@@ -53,40 +53,29 @@ async function getFolders(req, res) {
 async function updateFolder(req, res) {
   try {
     
-    const {id } = req.params;
-    let schema = Joi.number().positive().required();
-    await schema.validateAsync(id);
-
-    const {nombre} = req.body;
+    const {folderId} = req.params;
+   
+    const {nombre,newNombre} = req.body;
+    const selectquery ='SELECT * FROM carpetas WHERE id=?';
+    const [rows]= await database.pool.query(selectquery,folderId);  
   
-    schema = Joi.object({
-      nombre: Joi.string().required(),
-      
-    });
-
-    await schema.validateAsync({nombre});
-    
-
-    
-    const query = 'SELECT * FROM carpetas WHERE id = ?';
-    const [rows] = await database.pool.query(query,id);
-
     if (!rows || !rows.length) {
       res.status(404);
       return res.send({ error: 'Carpeta no encontrada' });
     }
     
-    const updateQuery = 'UPDATE carpetas SET nombre = ?  WHERE id = ?';
-    await database.pool.query(updateQuery, [nombre,id]);
-  
-  
+ 
     
-      
-  
-    
-    
+    const updateQuery = 'UPDATE carpetas SET nombre=?  WHERE id = ?';
+    await database.pool.query(updateQuery, [newNombre,folderId]);
+    fs.rename (`./controllers/carpetas_usuarios/${nombre}`,`./controllers/carpetas_usuarios/${newNombre}`); 
+    const newruta = path.join(__dirname,`./carpetas_usuarios/${newNombre}`);
+    const newQuery = 'UPDATE carpetas SET ruta=?  WHERE id = ?';
+    await database.pool.query(newQuery, [newruta,folderId]);
+       
     const selectQuery = 'SELECT * FROM carpetas WHERE id = ?';
-    const [selectRows] = await database.pool.query(selectQuery,id);
+    const [selectRows] = await database.pool.query(selectQuery,folderId);
+
 
     res.send(selectRows[0]);
   } catch (err) {
