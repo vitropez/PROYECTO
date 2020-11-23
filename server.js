de42b8ab3af6e3f7e94c93403ddd9db1c7c5ed5c
vitropez <64587299+vitropez@users.noreply.github.com>
2020-11-23 20:56:04 +0100

    Add files via upload

--- server.js
+++ server.js
@@ -19,13 +19,14 @@ app.use(fileUpload());
 app.use(bodyParser.json());
 
 // Endpoints / Rutas
-app.get('/api/folders', foldersController.getFolders);
+app.get('/api/folders/:folderId', foldersController.getFolders);
 app.get('/api/users', UsersController.getUsers);
 app.post('/api/users', UsersController.createUser);
 app.post('/api/users/login', UsersController.login);
 app.post('/api/folders/:userId',foldersController.createFolder);
 app.post('/api/ficheros/:folderId',ficherosControllers.createFichero);
 app.put('/api/folders/:folderId',foldersController.updateFolder);
+app.put('/api/ficheros/:folderId',ficherosControllers.updateficheros);
 
 // Escuchar un puerto
 app.listen(HTTP_PORT, () => console.log(`Escuchando en el puerto ${HTTP_PORT}`));
