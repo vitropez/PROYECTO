8ca79c848f084d9602abb70f5ffa6c0d93429e89
vitropez <64587299+vitropez@users.noreply.github.com>
2020-12-12 21:01:43 +0100

    Add files via upload

--- server.js
+++ server.js
@@ -2,7 +2,9 @@ require('dotenv').config();
 const express = require('express');
 const bodyParser = require('body-parser');
 const fileUpload = require('express-fileupload');
-
+const cors =require ('cors');
+var multer  = require('multer')
+var upload = multer()
 
 
 const {
@@ -10,22 +12,26 @@ const {
   UsersController,
 } = require('./controllers');
 const ficherosControllers = require('./controllers/ficheros-controllers');
+const { validateAuthorization } = require('./middlewares');
 
 const { HTTP_PORT } = process.env;
 
 const app = express();
-app.use(fileUpload());
-
+//app.use(fileUpload());
 app.use(bodyParser.json());
+app.use(cors());
+app.use('/static', express.static(__dirname + '/controllers/carpetas_usuarios'));
+
 
 // Endpoints / Rutas
-app.get('/api/folders/:folderId', foldersController.getFolders);
+app.get('/api/ficheros/:foldersId',ficherosControllers.getFicherosById)
+app.get('/api/folders',validateAuthorization, foldersController. getFoldersByUserId);
 app.get('/api/users', UsersController.getUsers);
-app.post('/api/users', UsersController.createUser);
+app.post('/api/users',upload.single('foto'), UsersController.createUser);
 app.post('/api/users/login', UsersController.login);
-app.post('/api/folders/:userId',foldersController.createFolder);
-app.post('/api/ficheros/:folderId',ficherosControllers.createFichero);
-app.put('/api/folders/:folderId',foldersController.updateFolder);
+app.post('/api/folders/:userId',validateAuthorization,foldersController.createFolder);
+app.post('/api/ficheros',ficherosControllers.createFichero);
+app.put('/api/folders/:folderId',validateAuthorization,foldersController.updateFolder);
 app.put('/api/ficheros/:folderId',ficherosControllers.updateficheros);
 
 // Escuchar un puerto
